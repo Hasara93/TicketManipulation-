982d5caa52dc934082b4c3313feb2193406c5597
Hasara <hja@codehousegroup.com>
2020-02-19 18:15:00 +0530

    1: refactoring the code

--- TicketManipulation_BootCampAssignment/TicketManipulation_BootCampAssignment/Program.cs
+++ TicketManipulation_BootCampAssignment/TicketManipulation_BootCampAssignment/Program.cs
@@ -1,19 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TicketManipulation_BootCampAssignment
 {
-    class Program
+    public class Program
     {
-        private  
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("**** Welcome To The System ****");
-            Console.ReadLine();            
-
-        }
+            Console.ReadLine();}
     }
 }
